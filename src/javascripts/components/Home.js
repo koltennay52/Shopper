a7ed4cd6c462b4aac05777b76782125c80369078
koltennay52 <60151320+koltennay52@users.noreply.github.com>
2021-08-16 16:59:56 -0600

    constructing home component

--- src/javascripts/components/Home.js
+++ src/javascripts/components/Home.js
@@ -2,7 +2,22 @@ import React, { useEffect, useState } from "react";
 import { LoadingSpinner } from "./LoadingSpinner";
 
 export function Home(props) {
+  const [pantry, setPantry] = useState();
 
+  //retrieve summoner name
+  useEffect(() => {
+    if (!pantry) {
+      fetch("/api/pantry", {
+        credentials: "same-origin",
+      })
+        .then((response) => response.text())
+        .then((data) => {
+          const retrieved_baskets = JSON.parse(data);
+          setPantry(retrieved_baskets);
+          console.log(retrieved_baskets)
+        });
+    }
+  });
 
     return (
       <>
